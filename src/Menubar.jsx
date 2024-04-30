6dcb029c4e55ae669faf3b950b6284d2575739c5
Dragon75240 <stevyn034@gmail.com>
2024-04-30 07:02:41 -0600

    formatting, cleaning up, more styling

--- src/Menubar.jsx
+++ src/Menubar.jsx
@@ -1,83 +1,61 @@
-import { ConfigProvider, theme } from "antd";
-import { useState } from "react";
+import "./styles/Menubar.css";
 import { Menu } from "antd";
 
-const items = [
-  {
-    label: "Navigation One",
-    key: "mail",
-  },
-  {
-    label: "Navigation Two",
-    key: "app",
-    disabled: true,
-  },
-  {
-    label: "Navigation Three - Submenu",
-    key: "SubMenu",
-    children: [
-      {
-        type: "group",
-        label: "Item 1",
-        children: [
-          { label: "Option 1", key: "setting:1" },
-          { label: "Option 2", key: "setting:2" },
-        ],
-      },
-      {
-        type: "group",
-        label: "Item 2",
-        children: [
-          { label: "Option 3", key: "setting:3" },
-          { label: "Option 4", key: "setting:4" },
-        ],
-      },
-    ],
-  },
-  {
-    key: "alipay",
-    label: (
-      <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-        Navigation Four - Link
-      </a>
-    ),
-  },
-];
 
-function Menubar() {
-  const [current, setCurrent] = useState("mail");
-
-  const { darkAlgorithm } = theme;
-
-  const onClick = (e) => {
-    console.log("click ", e);
-    setCurrent(e.key);
-  };
-
-  const renderLeftItem = () => (
-    <Menu.Item key="leftItem" onClick={onClick}>
-      Left Item
-    </Menu.Item>
-  );
+const App = () => {
+  const menuItems = [
+    {
+      key: "home",
+      label: "Stevyn Shawcroft",
+    },
+    {
+      type: "divider",
+      style: { flexGrow: 1, order: 1 },
+    },
+    {
+      key: "cover",
+      label: "Cover",
+    },
+    {
+      key: "abtme",
+      label: "About Me",
+    },
+    {
+      key: "hobbies",
+      label: "Hobbies",
+    },
+    {
+      key: "links",
+      label: "Links",
+    },
+    {
+      key: "path",
+      label: "Pathways"
+    },
+    {
+      key: "career",
+      label: "School Career"
+    },
+    {
+      key: "skill",
+      label: "Skills"
+    }
+  ];
 
   return (
-    <ConfigProvider
-      theme={{
-        algorithm: darkAlgorithm,
-      }}
-    >
-      <div className="flex justify-between items-center">
-        <div className="flex justify-end w-full">
-          <Menu
-            onClick={onClick}
-            selectedKeys={[current]}
-            mode="horizontal"
-            items={[renderLeftItem(), ...items]}
-          />
-        </div>
-      </div>
-    </ConfigProvider>
+    <Menu mode="horizontal" className="menubar backdrop:inset-10">
+      {menuItems.map((item, index) => {
+        if (item.type === "divider") {
+          return <Menu.Divider key={index} style={item.style} />;
+        }
+        return (
+          <Menu.Item key={item.key} icon={item.icon}>
+            {item.label}
+          </Menu.Item>
+        );
+      })}
+    </Menu>
   );
-}
+};
 
-export default Menubar;
+export default App;

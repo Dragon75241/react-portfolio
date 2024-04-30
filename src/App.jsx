ff7fcff101014d5b0a0eef6856d37493fb271d2d
Dragon75241 <166435424+Dragon75241@users.noreply.github.com>
2024-04-30 16:32:29 +0000

    background

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import "./styles/App.css";
 
 function App() {
   return (
-    <>
-    </>
+    <div>
+      <div className="background backdrop:inset-10" >
+
+      </div>
+    </div>
   );
 }
 export default App;
